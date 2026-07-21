9f04c83c0707407d935f948fe10fa04ca76b40b2
mohamedfaheem0907-sudo <mohamedfaheem0907@gmail.com>
2026-07-21 11:28:14 +0530

    Create day1 1295.java

---
+++ day1 1295.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int findNumbers(int[] nums) {
+        int count = 0;
+        for (int num : nums) {
+            // 1. Determine the number of digits in 'num'
+            // 2. If the count is even, increment the 'count' variable
+        }
+        return count;
+    }
+}
